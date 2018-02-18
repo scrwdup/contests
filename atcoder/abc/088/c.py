f4e4d22bb6e2195898a758fc160f5f74ecbea0aa
scrwdup <scrwdup@tutanota.com>
2018-02-18 21:16:40 +0000

    Fix solution to the problem C of ABC088

--- atcoder/abc/088/c.py
+++ atcoder/abc/088/c.py
@@ -5,11 +5,15 @@ import sys
 
 def main():
     c = tuple(tuple(map(int, line.split())) for line in sys.stdin.readlines())
-    csum = sum((sum(ci) for ci in c))
-    trace = sum((c[i][i] for i in range(len(c))))
-    if csum == 3 * trace:
-        print('Yes')
-    else:
-        print('No')
+    b = c[0]
+    a = [None] * 3
+    for i in range(3):
+        a[i] = c[i][i] - b[i]
+        for j in range(3):
+            if a[i] + b[j] != c[i][j]:
+                print('No')
+                sys.exit()
+    print('Yes')
+
 if __name__ == '__main__':
     main()
